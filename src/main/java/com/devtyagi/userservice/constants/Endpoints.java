1d1ac987c197e99e606734795be6e356762195d0
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2022-02-07 23:52:36 +0530

    Endpoints Documentation

--- src/main/java/com/devtyagi/userservice/constants/Endpoints.java
+++ src/main/java/com/devtyagi/userservice/constants/Endpoints.java
@@ -5,13 +5,16 @@ import lombok.NoArgsConstructor;
 
 public class Endpoints {
 
+    // Base URL for all endpoints
     public static final String BASE_URL = "/api/v1";
 
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static class AuthAPI {
 
+        // Base URL for Auth APIs
         public static final String AUTH_BASE_URL = "/auth";
 
+        // Login Endpoint
         public static final String LOGIN_USER = AUTH_BASE_URL + "/login";
 
     }
@@ -19,14 +22,19 @@ public class Endpoints {
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static class UserAPI {
 
+        // Base URL for User APIs
         public static final String USER_BASE_URL = "/user";
 
+        // [POST] Create User Endpoint
         public static final String CREATE_USER = USER_BASE_URL + "/create";
 
+        // [GET] Get all users Endpoint
         public static final String GET_USERS = USER_BASE_URL + "/all";
 
+        // [DELETE] Delete user endpoint
         public static final String DELETE_USER = USER_BASE_URL + "/delete/{id}";
 
+        // [PUT] Activate (Update Status and Password) Endpoint.
         public static final String ACTIVATE_USER = USER_BASE_URL + "/activate";
 
     }
